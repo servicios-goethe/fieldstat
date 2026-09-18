23489e27b49fc5baa4cce331f95ccf30587da247
servicios-goethe <servicios@goethe.edu.ar>
2026-09-18 16:13:49 +0000

    fix: use PKCE for Google sign-in

--- src/data/supabase.ts
+++ src/data/supabase.ts
@@ -13,5 +13,6 @@ export const supabase = createClient<Database>(url, anonKey, {
     persistSession: true,
     autoRefreshToken: true,
     detectSessionInUrl: true,
+    flowType: 'pkce',
   },
 })
